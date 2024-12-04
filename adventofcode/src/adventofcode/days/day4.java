fbde591649569887d5f9df6daaeff6b344812bf6
PenguGit <160494880+PenguGit@users.noreply.github.com>
2024-12-04 22:54:49 +0100

    I am struggling

--- adventofcode/src/adventofcode/days/day4.java
+++ adventofcode/src/adventofcode/days/day4.java
@@ -16,12 +16,14 @@ public class day4 {
 
     private static final ArrayList<String> lines = Input.readFile("testfile");
     private static int result = 0;
+
     public static void runFirst() {
         for (int row = 0; row < lines.size(); row++) {
             String line = lines.get(row);
             for (int letter = 0; letter < line.length(); letter++) {
                 if (line.charAt(letter) == 'X') {
-                    findXmas('X', row ,letter);
+                    System.out.println("Reading");
+                    findXmas('X', row, letter);
                 }
             }
         }
@@ -30,31 +32,47 @@ public class day4 {
     private static void findXmas(char curr, int row, int letter) {
         switch (curr) {
             case 'X' -> {
-                for (int r = -1; r < 2; r ++) {
-                    if (row == 0 && r == -1) {
-                        System.out.println("Hi");
-                        r++;
-                    }
-                    for(int c = -1; c < 2; c++) {
-                        if(letter == 0 && c == -1) {
-                            System.out.println("Ho");
-                            c++;
-                        }
-                        char checkLetter = lines.get(row + r).charAt(letter + c) ;
-                        System.out.println(checkLetter);
-                    }
-                }
+                System.out.println("X");
+                checkXmas('M', row, letter);
             }
             case 'M' -> {
-                
+                System.out.println("M");
+                checkXmas('A', row, letter);
             }
             case 'A' -> {
-                
+                System.out.println("A");
+                checkXmas('S' , row, letter);
             }
             case 'S' -> {
                 result++;
             }
-            default -> throw new AssertionError();
+            default ->
+                throw new AssertionError();
         }
+    }
+    
+    
+    private static void checkXmas(char curr, int row, int letter) {
+        for (int r = row - 1; r <= row + 1; r++) {
+                    if (r < 0) {
+                        r++;
+                    } else if (r == lines.size()) {
+                        break;
+                    }
+                    for (int c = letter - 1; c <= letter + 1; c++) {
+                        if (c < 0) {
+                            c++;
+                        } else if (c == lines.size()) {
+                            break;
+                        }
+                        System.out.println(r + " " + c);
+                        char checkLetter = lines.get(r).charAt(c);
+                        if (checkLetter == curr) {
+                            findXmas(curr, row+r, letter+c);
+                            return;
+                        }
+                    }
+                }
+                System.out.println("Done");
     }
 }
